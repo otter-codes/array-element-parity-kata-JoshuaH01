330aeb2060c551c6b703a41a3065c702c9d1209e
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-05-24 07:35:45 +0100

    all tests passing

--- src/main/scala/Katas.scala
+++ src/main/scala/Katas.scala
@@ -1,6 +1,6 @@
 object Katas extends App {
 
   def arrayelementparity(numbers: List[Int]) : Int = {
-    numbers.foldRight(0)(_ + _)
+    numbers.distinct.foldRight(0)(_+_)
   }
 }
--- src/test/scala/KatasSpec.scala
+++ src/test/scala/KatasSpec.scala
@@ -12,13 +12,17 @@ class KatasSpec extends WordSpec with MustMatchers {
       Katas.arrayelementparity(List(1,2)) mustEqual 3
     }
 
-    "When called must take a list of multiple ints and return the correct answer" in {
+    "When called must take a list of multiple ints and return the correct answer(kataTest1)" in {
       Katas.arrayelementparity(List(1, -1, 2, -2, 3)) mustEqual 3
     }
 
-    "When called must take a list of multiple ints and return the correct answer" in {
+    "When called must take a list of multiple ints and return the correct answer(kataTest2)" in {
       Katas.arrayelementparity(List(-3, 1, 2, 3, -1, -4, -2)) mustEqual -4
     }
 
+    "When called must take a list of multiple ints with possible doubles and return the correct answer(kataTest3)" in {
+      Katas.arrayelementparity(List(1, -1, 2, -2, 3, 3)) mustEqual 3
+
+    }
   }
 }
